b14c6a531ac55d7f5392b9d4221342ef0383cdc7
Gert Müller <gertmuller88@gmail.com>
2018-04-24 19:18:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 function startTimer(duration, display) {
-  var timer = duration, minutes, seconds;
+  var timer = duration, seconds;
   setInterval(function () {
-    minutes = parseInt(timer / 60, 10)
-    seconds = parseInt(timer % 60, 10);
-    
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-    
-    display.textContent = minutes + ":" + seconds;
+    display.textContent = seconds;
     
     if(--timer < 0) {
       timer = duration;
